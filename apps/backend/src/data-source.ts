7ae70b491890483b54c8a429de97ca072e870c07
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2026-09-13 14:44:16 -0400

    Using containerized postgres and resolving dependency issues

--- apps/backend/src/data-source.ts
+++ apps/backend/src/data-source.ts
@@ -6,11 +6,14 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.NX_DB_HOST,
-  port: parseInt(process.env.NX_DB_PORT as string, 10),
-  username: process.env.NX_DB_USERNAME,
-  password: process.env.NX_DB_PASSWORD,
-  database: process.env.NX_DB_DATABASE,
+  host: process.env.NX_DB_HOST ?? process.env.POSTGRES_HOST ?? 'localhost',
+  port: parseInt(
+    process.env.NX_DB_PORT ?? process.env.POSTGRES_PORT ?? '5432',
+    10,
+  ),
+  username: process.env.NX_DB_USERNAME ?? process.env.POSTGRES_USER,
+  password: process.env.NX_DB_PASSWORD ?? process.env.POSTGRES_PASSWORD,
+  database: process.env.NX_DB_DATABASE ?? process.env.POSTGRES_DB,
   entities: [],
   migrations: ['apps/backend/src/migrations/*.js'],
   // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data
