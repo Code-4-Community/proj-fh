db18cca9669d139a3bf816e791109e2fa2ebb2fc
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2026-09-15 09:43:30 -0400

    Tag migration

--- apps/backend/src/data-source.ts
+++ apps/backend/src/data-source.ts
@@ -4,6 +4,13 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+/**
+ * The AppDataSource is a TypeORM DataSource instance configured for connecting to a PostgreSQL database.
+ * It uses environment variables to determine the database connection parameters, including host, port, username, password, and database name.
+ * The DataSource is set up with a custom naming strategy (PluralNamingStrategy) and specifies the entities and migrations to be used.
+ * 
+ * Note: The synchronize option is set to false to prevent automatic schema synchronization in production environments.
+ */
 const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.NX_DB_HOST ?? process.env.POSTGRES_HOST ?? 'localhost',
@@ -14,7 +21,7 @@ const AppDataSource = new DataSource({
   username: process.env.NX_DB_USERNAME ?? process.env.POSTGRES_USER,
   password: process.env.NX_DB_PASSWORD ?? process.env.POSTGRES_PASSWORD,
   database: process.env.NX_DB_DATABASE ?? process.env.POSTGRES_DB,
-  entities: [],
+  entities: ['apps/backend/src/tags/tags.entity.ts'],
   migrations: ['apps/backend/src/migrations/*.js'],
   // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data
   synchronize: false,
---
+++ apps/backend/src/migrations/1789479716330-add_tag_entity.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class add_tag_entity1789479716330 implements MigrationInterface {
+    name = 'add_tag_entity1789479716330'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."tags_category_enum" AS ENUM('food_service_type', 'food_type', 'nutrition_program', 'access_requirement', 'housing_service_type', 'population_focus', 'price', 'eligibility_scope', 'accepts_ebt', 'transportation', 'language', 'other')`);
+        await queryRunner.query(`CREATE TABLE "tags" ("tag_id" integer NOT NULL, "category" "public"."tags_category_enum" NOT NULL, "label" character varying(255) NOT NULL, "slug" character varying(255) NOT NULL, CONSTRAINT "PK_06a35221325edeb80ad2ec1ff85" PRIMARY KEY ("tag_id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "tags"`);
+        await queryRunner.query(`DROP TYPE "public"."tags_category_enum"`);
+    }
+
+}
