1d27ae0aad04326db4ec3a1cfb1ee4ad0a289f69
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2026-09-17 12:59:08 -0400

    Stubbed Score Module (#12)
    * Stubbed Score Module
    
    * format

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
